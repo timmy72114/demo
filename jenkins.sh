2f5462bcf6c8e542d353b72f747795bc736cf42d
timmy72114 <timmy72114@gmail.com>
2022-12-09 17:43:55 +0800

    123

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+# git pull
+# git delete wheel file
+# build
+# delete orignal file
+cd /home/jw/Desktop/git/test/demo && sudo git fetch
+cd /home/jw/Desktop/git/test/demo && sudo git pull --rebase
+cd /home/jw/Desktop/git/test/demo && pyinstaller -F app.py
+cp /home/jw/Desktop/git/test/demo/dist/app && /home/jw/Desktop/
