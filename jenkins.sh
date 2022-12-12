d79bef9458e982c7c6c4de21a938ec4343b65e2c
timmy72114 <timmy72114@gmail.com>
2022-12-12 17:31:30 +0800

    ss

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,11 @@
 # git fetch
-sudo cd /home/jw/Desktop/git/test/demo && git fetch
+cd /home/jw/Desktop/git/test/demo && sudo git fetch
 # git pull
-sudo cd /home/jw/Desktop/git/test/demo && git pull --rebase
+cd /home/jw/Desktop/git/test/demo && sudo git pull --rebase
 # delete orignal file
-sudo cd /home/jw/Desktop/git/test/demo &&  rm -rf dist/
+cd /home/jw/Desktop/git/test/demo && sudo  rm -rf dist/
 # cd /home/jw/Desktop/git/test/demo && pip install --no-cache-dir -r requirements.txt
 # build
-sudo cd /home/jw/Desktop/git/test/demo && pyinstaller -F app.py
+cd /home/jw/Desktop/git/test/demo && sudo pyinstaller -F app.py
 # move file
-sudo cp -f /home/jw/Desktop/git/test/demo/dist/app /home/jw/Desktop/
+cp -f /home/jw/Desktop/git/test/demo/dist/app /home/jw/Desktop/
