1b4d3027570bdf43bcb529ddec62325b68868df2
timmy72114 <timmy72114@gmail.com>
2022-12-12 16:30:50 +0800

    CCC

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ cd /home/jw/Desktop/git/test/demo && sudo git fetch
 # git pull
 cd /home/jw/Desktop/git/test/demo && sudo git pull --rebase
 # delete orignal file
-# cd /home/jw/Desktop/git/test/demo/dist/ && rm app
+cd /home/jw/Desktop/git/test/demo/dist/ && rm app
 cd /home/jw/Desktop/git/test/demo && pip install --no-cache-dir -r requirements.txt
 # build
 cd /home/jw/Desktop/git/test/demo && sudo pyinstaller -F app.py
