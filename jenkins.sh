03a4f8fe1ecae497e2dcfc1355e159a841f3c146
timmy72114 <timmy72114@gmail.com>
2022-12-12 11:27:05 +0800

    update sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,10 @@
-# git pull
-# git delete wheel file
-# build
-# delete orignal file
+# git fetch
 cd /home/jw/Desktop/git/test/demo && sudo git fetch
+# git fetch
 cd /home/jw/Desktop/git/test/demo && sudo git pull --rebase
+# delete orignal file
+cd /home/jw/Desktop/git/test/demo/dist/ && rm app
+# build
 cd /home/jw/Desktop/git/test/demo && pyinstaller -F app.py
+# move file
 cp /home/jw/Desktop/git/test/demo/dist/app && /home/jw/Desktop/
