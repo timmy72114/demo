740090ffad1ff90d34a9f57ae090f3051207ea9e
timmy72114 <timmy72114@gmail.com>
2022-12-12 17:14:30 +0800

    ZXC

--- jenkins.sh
+++ jenkins.sh
@@ -3,8 +3,8 @@ cd /home/jw/Desktop/git/test/demo && sudo git fetch
 # git pull
 cd /home/jw/Desktop/git/test/demo && sudo git pull --rebase
 # delete orignal file
-cd /home/jw/Desktop/git/test/demo/dist/ && sudo rm -f app
-cd /home/jw/Desktop/git/test/demo && pip install --no-cache-dir -r requirements.txt
+cd /home/jw/Desktop/git/test/demo/dist && sudo rm -f app
+# cd /home/jw/Desktop/git/test/demo && pip install --no-cache-dir -r requirements.txt
 # build
 cd /home/jw/Desktop/git/test/demo && sudo pyinstaller -F app.py
 # move file
